498a9f6781da2de29168b3b6e071ad40a62052d8
Kit Langton <kitlangton@gmail.com>
2016-07-05 21:25:12 -0400

    change verbiage

--- lib/au_pair/cli.rb
+++ lib/au_pair/cli.rb
@@ -7,7 +7,7 @@ module AuPair
     def add(name)
       setup = AuPair::Setup.new
       setup.add name
-      puts "You can now pull from #{name.capitalize}'s repo with 'git pull #{name}'"
+      puts "You can now pull from #{name.capitalize}'s repo with 'git pull #{name} master'"
     end
   end
 end
